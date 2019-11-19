bd57c4e0b84c5bae635373e8ec3c1f5685796ab6
sangwoo0727 <kangsw9395@gmail.com>
2019-11-20 03:34:04 +0900

    create background.js

---
+++ js/background.js
@@ -0,0 +1,15 @@
+const UNSPLASH_API_KEY = "https://source.unsplash.com/random";
+
+const body = document.querySelector("body");
+
+
+function loadBackground(){
+    //아직 완성 못함..
+    
+}
+
+function initApp(){
+    loadBackground();
+}
+
+initApp();
